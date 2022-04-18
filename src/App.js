da99e407d9dc85779f38db6b75d612deed1a78cd
James Cole <jcole099@hotmail.com>
2022-04-18 02:35:33 -0700

    week 5 complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const year = new Date().getFullYear();
   const [userInput, setUserInput] = useState('');
   var userData = [];
-  let convertInfix = false; //used for validation of parenthesis. Set to true if user is trying to convert Infix -> Postfix TODO: implement functionality to change this value when tabs change
+  let convertInfix = true; //used for validation of parenthesis. Set to true if user is trying to convert Infix -> Postfix TODO: implement functionality to change this value when tabs change
 
   function isNumber(char) {
     if (
@@ -60,6 +60,20 @@ function App() {
     }
   }
 
+  function isStrictOperator(char) {
+    if (
+      char === '+' ||
+      char === '-' ||
+      char === '*' ||
+      char === '/' ||
+      char === '^'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   function isPar(char) {
     if (char === '(' || char === ')') {
       return true;
@@ -82,7 +96,7 @@ function App() {
     let previousEl = null;
     for (let char of userInput) {
       if (isPar(char) && !convertInfix) {
-        console.error(`Parenthesis are not allowed in this operation`);
+        displayError(`Parenthesis are not allowed in this operation`);
         break;
       }
       if (isNumber(char)) {
@@ -90,7 +104,7 @@ function App() {
         //case with multiple consecutive periods
         if (previousEl !== null) {
           if (char === '.' && previousEl[previousEl.length - 1] === '.') {
-            console.error(
+            displayError(
               `Invalid Syntax. Cannot have 2 or more periods next to each other.`
             );
             break;
@@ -138,12 +152,12 @@ function App() {
         //VALIDATION
         //if prevnum is null, if char is not a parenthesis,
         if (previousEl === null && !isPar(char)) {
-          console.error(`First value cannot be an operator [${char}]`);
+          displayError(`First value cannot be an operator [${char}]`);
           break;
         }
         //also invalid if prev = null, is parenthesis, is not convertInfix
         if (previousEl === null && isPar(char) && !convertInfix) {
-          console.error(`First value cannot be an operator [${char}]`);
+          displayError(`First value cannot be an operator [${char}]`);
           break;
         }
         //handles case where there is no space between number and operator
@@ -216,15 +230,15 @@ function App() {
           } else if ((char === '(' || char === ')') && convertInfix) {
             continue;
           } else {
-            console.error(`Invalid syntax! [${char}] is an illegal character`);
+            displayError(`Invalid syntax! [${char}] is an illegal character`);
           }
         }
       } else {
         //NOT VALID
-        console.error(`Not a valid char! [${char}]`);
-        console.error(`No letters allowed`);
-        console.error(`No commas allowed`);
-        console.error(`Only use the following operators: + - * / ^ ( )`);
+        displayError(`Not a valid char! [${char}]`);
+        displayError(`No letters allowed`);
+        displayError(`No commas allowed`);
+        displayError(`Only use the following operators: + - * / ^ ( )`);
         //TODO: Display errors on the GUI
         return;
       }
@@ -234,7 +248,7 @@ function App() {
     console.log(`DL User Data: ${userData}`);
 
     //calculatePostfix(userData, false);
-    convertInfix(userData);
+    convertInfixFunc(userData);
   };
 
   /////////////////////////////////////////////
@@ -245,7 +259,6 @@ function App() {
   // Second parameter is boolean, true = calculate | false = convert
   //
   function calculatePostfix(dataArray, calculate) {
-    //TODO: check 10 5 + 7 - 8 conversion
     let stack = [];
     let topEl;
     let bottomEl;
@@ -261,10 +274,10 @@ function App() {
         if (i === dataArray.length - 1) {
           //VALIDATION
           if (stack.length > 1) {
-            console.error(
+            displayError(
               `Invalid Postfix syntax - Multiple items on stack at end of process`
             );
-            console.error(`CP STACK: ${stack}`);
+            displayError(`CP STACK: ${stack}`);
           } else {
             console.log(`CP Result: ${stack[0]}`);
           }
@@ -275,17 +288,17 @@ function App() {
 
         //VALIDATION - Verify that there are two elements on stack
         if (stack.length < 2) {
-          console.error(`Invalid Postfix syntax - Need two elements on Stack`);
-          console.error(`CP STACK: ${stack}`);
+          displayError(`Invalid Postfix syntax - Need two elements on Stack`);
+          displayError(`CP STACK: ${stack}`);
         }
 
         if (i === dataArray.length - 1) {
           //VALIDATION
           if (!isNaN(dataArray[i])) {
-            console.error(
+            displayError(
               `Invalid Postfix syntax - Last element must be operator`
             );
-            console.error(`CP STACK: ${stack}`);
+            displayError(`CP STACK: ${stack}`);
           }
         }
 
@@ -325,10 +338,10 @@ function App() {
         if (i === dataArray.length - 1) {
           //VALIDATION
           if (stack.length > 1) {
-            console.error(
+            displayError(
               `Invalid Postfix syntax - Multiple items on stack at end of process`
             );
-            console.error(`CP STACK: ${stack}`);
+            displayError(`CP STACK: ${stack}`);
           } else {
             console.log(`CP Result: ${stack[0]}`);
           }
@@ -365,27 +378,161 @@ function App() {
     return val1Rank > val2Rank;
   }
 
-  function convertInfix(dataArray) {
+  function hasConsecutiveOperators(dataArray) {
+    let operatorDetected = false;
+    let consecutive = false;
+
+    for (let element of dataArray) {
+      if (isStrictOperator(element)) {
+        if (operatorDetected) {
+          return true;
+        } else {
+          operatorDetected = true;
+        }
+      } else {
+        operatorDetected = false;
+      }
+    }
+    return false;
+  }
+
+  function hasConsecutiveNumbers(dataArray) {
+    let numberDetected = false;
+
+    for (let element of dataArray) {
+      if (!isNaN(element)) {
+        if (numberDetected) {
+          return true;
+        } else {
+          numberDetected = true;
+        }
+      } else {
+        numberDetected = false;
+      }
+    }
+    return false;
+  }
+
+  function isAttemptingImpliedMultiplication(dataArray) {
+    //check for numbers next to parenthesis
+    let numPresent = false;
+    let parenthPresent = false;
+    let closingParenthDetected = false;
+
+    for (let element of dataArray) {
+      //checks parenth * parenth
+      if (element === ')') {
+        closingParenthDetected = true;
+      } else if (element === '(' && closingParenthDetected) {
+        return true;
+      } else {
+        closingParenthDetected = false;
+      }
+      //checks num * parenth
+      if (!isNaN(element)) {
+        numPresent = true;
+      } else if (element === '(' && numPresent) {
+        return true;
+      } else {
+        numPresent = false;
+      }
+      //check parenth * num
+      if (element === ')') {
+        parenthPresent = true;
+      } else if (!isNaN(element) && parenthPresent) {
+        return true;
+      } else {
+        parenthPresent = false;
+      }
+    }
+    return false;
+  }
+
+  function convertInfixFunc(dataArray) {
     let stack = [];
     let result = '';
     let tempVal;
+    let hasOpeningParenthesis = false;
 
-    for (let i = 0; i < dataArray.length - 1; i++) {
+    //EARLY VALIDATION
+    if (hasConsecutiveOperators(dataArray)) {
+      displayError(
+        'Invalid Syntax - cannot have multiple consecutive operators'
+      );
+      return;
+    }
+
+    if (hasConsecutiveNumbers(dataArray)) {
+      displayError('Invalid Syntax - cannot have multiple consecutive numbers');
+      return;
+    }
+
+    if (isAttemptingImpliedMultiplication(dataArray)) {
+      displayError(
+        'Invalid Syntax - must explicitly include multiplication symbol'
+      );
+      return;
+    }
+
+    //CONVERSION
+    for (let i = 0; i < dataArray.length; i++) {
       let currentEl = dataArray[i];
 
       //End of input?
       if (i === dataArray.length - 1) {
         //Validation check #2
-        if (isNaN(currentEl)) {
-          console.error(
+        if (isNaN(currentEl) && currentEl !== ')') {
+          displayError(
             'Invalid Syntax - cannot have operator at the end of the input (for Infix)'
           );
           return;
+        } else if (currentEl === ')' && stack.length > 0) {
+          //handle case where closing parenth at end
+          //dump stack to result (excluding first element which is an opening parenth)
+          while (stack.length > 0) {
+            tempVal = stack.pop();
+            if (tempVal !== '(') {
+              result += ' ' + tempVal;
+            }
+            if (tempVal === '(' && hasOpeningParenthesis) {
+              displayError(
+                `Invalid Syntax - parenthesis imbalance (too many opening parentheses)`
+              );
+              return;
+            }
+            if (tempVal === '(') {
+              hasOpeningParenthesis = true;
+            }
+          }
+          if (!hasOpeningParenthesis) {
+            displayError(
+              `Invalid Syntax - parenthesis imbalance (too many closing parentheses)`
+            );
+            return;
+          }
+        } else if (currentEl === ')' && stack.length === 0) {
+          displayError(
+            `Invalid Syntax - parenthesis imbalance (too many closing parentheses)`
+          );
+          return;
+        } else {
+          //number
+          //put operands in result
+          result += ' ' + currentEl;
+
+          //dump stack to result
+          while (stack.length > 0) {
+            if (stack[stack.length - 1] === '(') {
+              displayError(
+                `Invalid Syntax - parenthesis imbalance (too many opening parentheses)`
+              );
+              return;
+            } else {
+              result += ' ' + stack.pop();
+            }
+          }
         }
 
-        for (let j = 0; j < stack.length - 1; j++) {
-          result += ' ' + stack.pop();
-        }
         break;
       }
 
@@ -400,16 +547,21 @@ function App() {
           continue;
         } else {
           if (currentEl === ')') {
+            //validation
+            if (currentEl === ')' && stack.length === 0) {
+              displayError(
+                `Invalid Syntax - parenthesis imbalance (too many closing parentheses)`
+              );
+              return;
+            }
             //loop through stack looking for opening parenth
-            for (let k = stack.length - 1; k > 0; k--) {
+            for (let k = stack.length - 1; k >= 0; k--) {
               if (stack[k] === '(') {
                 stack.pop(); //get rid of opening parenth
                 break; //exit inner loop
               } else {
                 if (k === 0) {
-                  console.error(
-                    `Invalid Syntax - No opening parenthesis found`
-                  );
+                  displayError(`Invalid Syntax - No opening parenthesis found`);
                   return;
                 }
                 tempVal = stack.pop();
@@ -418,8 +570,14 @@ function App() {
             }
             continue;
           } else {
+            //edge case where stack has no length
+            if (stack.length === 0) {
+              stack.push(currentEl);
+              continue; //next outer loop
+            }
+
             //Is currentEl higher precedence than top of the stack?
-            for (let l = stack.length - 1; l > 0; l--) {
+            for (let l = stack.length - 1; l >= 0; l--) {
               if (isHigherPrecedence(currentEl, stack[stack.length - 1])) {
                 stack.push(currentEl);
                 break; //exit inner loop
@@ -427,7 +585,7 @@ function App() {
                 tempVal = stack.pop();
                 result += ' ' + tempVal;
                 if (l === 0) {
-                  result += ' ' + currentEl;
+                  stack.push(currentEl);
                 }
               }
             }
@@ -436,6 +594,11 @@ function App() {
       }
     }
     console.log(`CI Result: ${result}`);
+  }
+
+  function displayError(msg) {
+    console.error(msg);
+    return;
   }
 
   return (
